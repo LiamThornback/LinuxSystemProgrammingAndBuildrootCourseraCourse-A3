5b8bc416859c9804f06a5061e97d4e56f8209f3e
user <user@example.com>
2025-04-14 02:27:58 -0300

    Assignment 1 completed

---
+++ finder-app/finder.sh
@@ -0,0 +1,14 @@
+if [ -z "$1" ]; then 
+        exit 1
+fi
+if [ -z "$2" ]; then 
+        exit 1
+fi
+if [ ! -d "$1" ]; then 
+        exit 1
+fi
+
+NUM_OF_FILES=$(find "$1" -type f | wc -l)
+NUM_OF_MATCHES=$(find "$1" -type f -exec grep "$2" {} \; | wc -l)
+
+echo "The number of files are $NUM_OF_FILES and the number of matching lines are $NUM_OF_MATCHES"
